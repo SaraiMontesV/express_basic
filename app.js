2caf94658d7382a624ad1a37b41c248101f4fe13
SaraiMontesV <sarizvargas@gmail.com>
2022-04-21 20:54:18 -0500

    Adding query params

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000 //localhost:3000
+const express = require("express");
+const app = express();
+const port = 3000; //localhost:3000
 
-app.get('/', (req, res) =>{
-    res.send("Hola a todos hfhfhfh")
-})
-
-// localhost:3000/launchx
-app.get('/launchx', (req, res) =>{
-    res.send("Bienvenidos a launch x")
-})
+app.get("/", (req, res) => {
+  res.send("Hola a todos hfhfhfh");
+});
 
 // localhost:3000/launchx
-app.get('/node', (req, res) =>{
-    const explorer1 = {id: 1, name: 'Sara'}
-    const explorer2 = {id: 2, name: 'Sara'}
-    const explorer3 = {id: 3, name: 'Sara'}
-    res.send([explorer1, explorer2, explorer3])
-})
+app.get("/launchx", (req, res) => {
+  res.send("Bienvenidos a launch x");
+});
 
-app.listen(port, ()=>{
-    console.log("Server listo")
-})
+// localhost:3000/launchx
+// curl localhost:3000/node
+app.get("/node", (req, res) => {
+  const explorer1 = { id: 1, name: "Sara" };
+  const explorer2 = { id: 2, name: "Sara" };
+  const explorer3 = { id: 3, name: "Sara" };
+  res.send([explorer1, explorer2, explorer3]);
+});
+
+//localhost:3000/explorers/Sarai
+app.get("/explorers/:explorer", (req, res) => {
+  console.log(req.params);
+  res.send(req.params);
+});
+
+app.listen(port, () => {
+  console.log("Server listo");
+});
