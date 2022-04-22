652315a34ec0b33a7abd3a58cb625f04f533cb30
SaraiMontesV <sarizvargas@gmail.com>
2022-04-21 20:52:01 -0500

    Adding new routes to my server

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ const app = express()
 const port = 3000 //localhost:3000
 
 app.get('/', (req, res) =>{
-    res.send("Hola a todos")
+    res.send("Hola a todos hfhfhfh")
+})
+
+// localhost:3000/launchx
+app.get('/launchx', (req, res) =>{
+    res.send("Bienvenidos a launch x")
+})
+
+// localhost:3000/launchx
+app.get('/node', (req, res) =>{
+    const explorer1 = {id: 1, name: 'Sara'}
+    const explorer2 = {id: 2, name: 'Sara'}
+    const explorer3 = {id: 3, name: 'Sara'}
+    res.send([explorer1, explorer2, explorer3])
 })
 
 app.listen(port, ()=>{
